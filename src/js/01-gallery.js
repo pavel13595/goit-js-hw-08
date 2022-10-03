562e9e7ffdf8dcea90f70ec5eba0a475ea4013c9
Pavel Mogilevskyi <pavel13595@gmail.com>
2022-10-03 19:30:32 +0300

    01-gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,6 +1,28 @@
-// Add imports above this line
-import SimpleLightbox from 'simplelightbox';
 import { galleryItems } from './gallery-items';
-// Change code below this line
-console.log(SimpleLightbox);
-console.log(galleryItems);
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import SimpleLightbox from 'simplelightbox';
+
+const refs = {
+  gallery: document.querySelector('.gallery'),
+};
+
+refs.gallery.innerHTML = createGalleryMarkup(galleryItems);
+
+let gallery = new SimpleLightbox('.gallery a', {
+  caption: true,
+  captionType: 'attr',
+  captionsData: 'alt',
+  captionDelay: '250ms',
+});
+
+function createGalleryMarkup(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
+                    <img class="gallery__image" 
+                        src="${preview}" 
+                        alt="${description}" />
+                    </a>`;
+    })
+    .join('');
+}
