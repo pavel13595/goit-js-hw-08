16b45e9113795c554b0f98e87139de41fd0f9a00
Pavel Mogilevskyi <pavel13595@gmail.com>
2022-10-04 18:49:29 +0300

    02/03

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,49 @@
+import throttle from 'lodash.throttle';
+import '../css/common.css';
+import '../css/03-feedback.css';
 
+const STORAGE_KEY = 'feedback-form-state';
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  textarea: document.querySelector('.feedback-form textarea'),
+  input: document.querySelector('input'),
+};
+
+const formData = {};
+
+populateTextarea();
+
+refs.form.addEventListener('input', throttle(onTextareaInput, 500));
+refs.form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  const form = event.currentTarget;
+  const formData = new FormData(form);
+  const finalData = {};
+  for (const [key, value] of formData.entries()) {
+    if (!value) {
+      alert('Все поля должны быть заполнены!!');
+      return;
+    }
+    finalData[key] = value;
+  }
+  console.log(finalData);
+  form.reset();
+  localStorage.removeItem(STORAGE_KEY);
+}
+
+function onTextareaInput(e) {
+  formData[e.target.name] = e.target.value;
+  const stringifiedData = JSON.stringify(formData);
+  localStorage.setItem(STORAGE_KEY, stringifiedData);
+}
+
+function populateTextarea() {
+  const savedMessage = JSON.parse(localStorage.getItem(STORAGE_KEY));
+  if (savedMessage === null) {
+    return;
+  }
+  refs.textarea.value = savedMessage['message'] || '';
+  refs.input.value = savedMessage['email'] || '';
+}
